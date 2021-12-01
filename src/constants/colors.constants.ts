4f2879e3b664599ccf2c5e64cd39723c1286a432
SOLBI1028 <hagkthfql@naver.com>
2021-12-01 12:55:36 +0900

    chore: add assets (icon, color constant)

---
+++ src/constants/colors.constants.ts
@@ -0,0 +1,10 @@
+export const Colors = {
+    primary: '#154C00', // 진초록
+    primary_lighter: '#D0DBCC', //연초록
+    accent: '#F9EFCC', //연노랑
+    darker: '#3A3A3A', // 진회색
+    dark: '#C4C4C4', //중간회색
+    light: '#B0B0B0', // 연회색
+    black: '#000',
+    white: '#fff'
+};
