438d4759dfcb8baa2b765fb524e2d443ab20d527
SOLBI1028 <hagkthfql@naver.com>
2021-12-01 16:41:06 +0900

    feat(font): Add font

--- src/providers/paper.tsx
+++ src/providers/paper.tsx
@@ -1,10 +1,68 @@
 import * as React from 'react';
 import {
+  configureFonts,
   DefaultTheme,
   Provider as ReactNativePaperProvider,
 } from 'react-native-paper';
 
 import {Colors} from '../constants';
+
+const fontConfig = {
+  web: {
+    regular: {
+      fontFamily: 'NanumSquareRoundB',
+      fontWeight: 'normal'
+    },
+    medium: {
+      fontFamily: 'NanumSquareRoundR',
+      fontWeight: 'normal'
+    },
+    light: {
+      fontFamily: 'NanumSquareRoundL',
+      fontWeight: 'bold'
+    },
+    thin: {
+      fontFamily: 'NanumSquareRoundL',
+      fontWeight: 'normal'
+    }
+  },
+  ios: {
+    regular: {
+      fontFamily: 'NanumSquareRoundB',
+      fontWeight: 'normal'
+    },
+    medium: {
+      fontFamily: 'NanumSquareRoundR',
+      fontWeight: 'normal'
+    },
+    light: {
+      fontFamily: 'NanumSquareRoundL',
+      fontWeight: 'bold'
+    },
+    thin: {
+      fontFamily: 'NanumSquareRoundL',
+      fontWeight: 'normal'
+    }
+  },
+  android: {
+    regular: {
+      fontFamily: 'NanumSquareRoundB',
+      fontWeight: 'normal'
+    },
+    medium: {
+      fontFamily: 'NanumSquareRoundR',
+      fontWeight: 'normal'
+    },
+    light: {
+      fontFamily: 'NanumSquareRoundL',
+      fontWeight: 'bold'
+    },
+    thin: {
+      fontFamily: 'NanumSquareRoundL',
+      fontWeight: 'normal'
+    }
+  }
+}
 
 const theme = {
   ...DefaultTheme,
@@ -16,6 +74,7 @@ const theme = {
     text: Colors.darker,
     placeHolder: Colors.light
   },
+  fonts: configureFonts(fontConfig),
 };
 
 export function PaperProvider({children}: {children: React.ReactNode}) {
