f723ab8c2f15f12b75ef13f5238c4b933c8d9d59
SOLBI1028 <hagkthfql@naver.com>
2021-12-06 23:30:26 +0900

    feat(screens) : Add splash screen
    - Add react-native-splash-screen, react-native-make library

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 
 import {PaperProvider, NavigationProvider} from './providers';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(()=> {
+    let timer = setTimeout(()=>{SplashScreen.hide();}, 0);
+    return () => {
+      clearTimeout(timer);
+    }
+  });
+
   return (
     <PaperProvider>
       <NavigationProvider/>
