fb0af3146971e523b780e2cc4f674e617c7cd8bd
cranemont <cranemont@gmail.com>
2022-01-09 04:18:46 +0900

    feat: Add backend API urls for development

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,122 +1,123 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://3.34.123.79'
+axios.defaults.baseURL = process.env.FNET_ENV ? 'http://3.34.123.79' : 
+                        Platform.OS === 'ios' ? 'http://127.0.0.1:8000' : 'http://10.0.2.2:8000'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 axios.defaults.xsrfCookieName = 'csrftoken'
 
 export default {
   login (data) {
-    return ajax('login/', 'post', {
+    return ajax('login', 'post', {
       data
     })
   },
   logout () {
-    return ajax('logout/', 'get')
+    return ajax('logout', 'get')
   },
   getFG (id) {
-    return ajax('fg/', 'get', {
+    return ajax('fg', 'get', {
       params: {
         id
       }
     })
   },
   getFGList (active) {
-    return ajax('fg/', 'get', {
+    return ajax('fg', 'get', {
       params: {
         active: active
       }
     })
   },
   uploadFGList (data) {
-    return ajax('admin/fg/', 'post', {
+    return ajax('admin/fg', 'post', {
       data
     })
   },
   getNotice (id) {
-    return ajax('notice/', 'get', {
+    return ajax('notice', 'get', {
       params: {
         id
       }
     })
   },
   getNoticeList () {
-    return ajax('notice/', 'get')
+    return ajax('notice', 'get')
   },
   createNotice (data) {
-    return ajax('admin/notice/', 'post', {
+    return ajax('admin/notice', 'post', {
       data
     })
   },
   editNotice (data) {
-    return ajax('admin/notice/', 'put', {
+    return ajax('admin/notice', 'put', {
       data
     })
   },
   deleteNotice (id) {
-    return ajax('admin/notice/', 'delete', {
+    return ajax('admin/notice', 'delete', {
       params: {
         id
       }
     })
   },
   getCommentList (noticeId) {
-    return ajax('notice/comment/', 'get', {
+    return ajax('notice/comment', 'get', {
       params: {
         notice_id: noticeId
       }
     })
   },
   createComment (data) {
-    return ajax('notice/comment/', 'post', {
+    return ajax('notice/comment', 'post', {
       data
     })
   },
   editComment (data) {
-    return ajax('notice/comment/', 'put', {
+    return ajax('notice/comment', 'put', {
       data
     })
   },
   deleteComment (id) {
-    return ajax('notice/comment/', 'delete', {
+    return ajax('notice/comment', 'delete', {
       params: {
         id
       }
     })
   },
   checkComment (data) {
-    return ajax('admin/notice/comment/', 'put', {
+    return ajax('admin/notice/comment', 'put', {
       data
     })
   },
   getLC (id) {
-    return ajax('lc/', 'get', {
+    return ajax('lc', 'get', {
       params: {
         id
       }
     })
   },
   getLCList () {
-    return ajax('lc/', 'get')
+    return ajax('lc', 'get')
   },
   createLC (data) {
-    return ajax('lc/', 'post', {
+    return ajax('lc', 'post', {
       data
     })
   },
   editLC (data) {
-    return ajax('lc/', 'put', {
+    return ajax('lc', 'put', {
       data
     })
   },
   deleteLC (id) {
-    return ajax('lc/', 'delete', {
+    return ajax('lc', 'delete', {
       params: {
         id
       }
     })
   },
   getLCMemberList (id, register) {
-    return ajax('freshman/lc/', 'get', {
+    return ajax('freshman/lc', 'get', {
       params: {
         id: id,
         register: register // null, undefined인 경우 제거됨. https://github.com/axios/axios/issues/1139
@@ -124,17 +125,17 @@ export default {
     })
   },
   getFreshman (id) {
-    return ajax('admin/freshman/', 'get', {
+    return ajax('admin/freshman', 'get', {
       params: {
         id
       }
     })
   },
   getFreshmanList () {
-    return ajax('admin/freshman/', 'get')
+    return ajax('admin/freshman', 'get')
   },
   uploadFreshman (data) {
-    return ajax('admin/freshman/file/', 'post', {
+    return ajax('admin/freshman/file', 'post', {
       data
     })
   },
