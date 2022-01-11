89d9426b2e38a637d7c539aad770dfefb97b242a
minseo <alstj778@gmail.com>
2022-01-12 00:37:38 +0900

    feat(screen): Add api for notice list

--- src/screens/notice/notice.tsx
+++ src/screens/notice/notice.tsx
@@ -1,59 +1,103 @@
-import React from 'react';
-import {View, StyleSheet, ScrollView, Pressable} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, TouchableOpacity, FlatList} from 'react-native';
 import {Text} from 'react-native-paper';
 import { basicStyles, GreenButton, Header } from '../../components';
 import {useNavigation} from '../../providers'
+import { useFocusEffect } from '@react-navigation/native';
+import api from '../../utils/api'
 
 export default function NoticeScreen() {
     const navigation = useNavigation();
     function navigate(){navigation.navigate('NoticeCreate')}
 
-    return(
-        <ScrollView>
-            <View style={basicStyles.container}>
-                <View style={styles.header}>
-                    <Header title='공지사항'/>
-                    <GreenButton text='+new' press={navigate}/>
-                </View>
+    const [noticeList, setNoticeList] = useState([])
+    const [refresing, setRefresing] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-                <View style={styles.notice_container}>
-                    <Pressable 
-                        style={styles.notice}
-                        onPress={()=>navigation.navigate('NoticeDetail')}    
-                    >
-                        <Text style={styles.notice_title}>공지합니다 하나 둘</Text>
-                        <Text style={basicStyles.lightText}>3분전</Text>
-                    </Pressable>
-                    <Pressable 
-                        style={styles.notice}
-                        onPress={()=>navigation.navigate('NoticeDetail')}    
-                    >
-                        <Text style={styles.notice_title}>공지합니다 하나 둘</Text>
-                        <Text style={basicStyles.lightText}>3분전</Text>
-                    </Pressable>
-                    <Pressable 
-                        style={styles.notice}
-                        onPress={()=>navigation.navigate('NoticeDetail')}    
-                    >
-                        <Text style={styles.notice_title}>공지합니다 하나 둘</Text>
-                        <Text style={basicStyles.lightText}>3분전</Text>
-                    </Pressable>
-                </View>
+    useFocusEffect(
+        React.useCallback(() => {
+            getNoticeList()
+        }, [])
+    )
+
+    async function getNoticeList() {
+        setLoading(true)
+        try {
+            const res = await api.getNoticeList()
+            setNoticeList(res.data.data.results)
+        } catch (err) {
+            alert(err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    function dateFormatter(date) {
+        const create_time = new Date(date)
+        const now = new Date()
+        var diff = (now.getTime() - create_time.getTime())/(1000*60)
+        if (diff<60) {
+            diff = Math.round(diff)
+            return diff + "분 전"
+        } else if (diff<60*24) {
+            diff = Math.round(diff/60)
+            return diff + "시간 전"
+        } else {
+            diff = Math.round(diff/(60*24))
+            return diff + "일 전"
+        }
+    }
+
+    const Item = ({item}) => (
+        <TouchableOpacity
+            style={styles.notice}
+            onPress={() => navigation.navigate('NoticeDetail', {id: item.id})}
+        >
+            <Text style={styles.notice_title}>{item.title}</Text>
+            <Text style={basicStyles.lightText}>{dateFormatter(item.create_time)}</Text>
+        </TouchableOpacity>
+    )
+
+    const renderItem = ({item}) => (
+        <Item
+            item={item}
+        />
+    )
+
+    //TODO: render new button only for admin
+    return(
+        <View style={basicStyles.container}>
+            <View style={styles.header}>
+                <Header title='공지사항'/>
+                <GreenButton text='+new' press={navigate}/>
             </View>
-        </ScrollView>
-        
+            <FlatList
+                ListEmptyComponent={
+                    <Text style={{textAlign: 'center'}}>No Notice</Text>
+                }
+                data={noticeList}
+                renderItem={renderItem}
+                keyExtractor={item => item.id.toString()}
+                style={styles.notice_list}
+                refreshing={refresing}
+                onRefresh={getNoticeList}
+            />
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     header: {
-        flex: 1,
         flexDirection: 'row',
         justifyContent: 'space-between',
         width: '100%'
     },
     notice_container: {
         margin: '5%'
+    },
+    notice_list: {
+        width: '100%',
+        padding: '5%'
     },
     notice: {
         width: '100%',
