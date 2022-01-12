efd082b57c37d75ec6f5541ba651b4d583668f2b
cranemont <cranemont@gmail.com>
2022-01-12 16:35:16 +0900

    test(components): Add an example of using paparTable

--- src/screens/register/register.tsx
+++ src/screens/register/register.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import {View, StyleSheet, ScrollView} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {View, StyleSheet, ScrollView, Text} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Header, InputForm, PaperTable } from '../../components';
 import {basicStyles} from '../../components/basic_styles'
 import { Colors } from '../../constants';
+import api from '../../utils/api';
 
 export default function RegisterScreen() {
-    const tableHeader = ['#', '이름', '전화번호', 'LC', '접수']
-
-    //TODO: API로 받아오기
-    const userInfo = [
-        {'id': 1, 'name': '김학산', 'phoneNumber': '8213', 'LC': 'LC10', 'registered': true},
-        {'id': 2, 'name': '김학산', 'phoneNumber': '9999', 'LC': 'LC11', 'registered': false},
-        {'id': 3, 'name': '김학산', 'phoneNumber': '1234', 'LC': 'LC11', 'registered': true},
-        {'id': 4, 'name': '김학산', 'phoneNumber': '2222', 'LC': 'LC16', 'registered': true},
-        {'id': 5, 'name': '김학산', 'phoneNumber': '9874', 'LC': 'LC10', 'registered': true},
-        {'id': 6, 'name': '김학산', 'phoneNumber': '2351', 'LC': 'LC14', 'registered': false},
-        {'id': 7, 'name': '김학산', 'phoneNumber': '8442', 'LC': 'LC10', 'registered': false},
-        {'id': 8, 'name': '김학산', 'phoneNumber': '9982', 'LC': 'LC11', 'registered': true},
-        {'id': 9, 'name': '김학산', 'phoneNumber': '9111', 'LC': 'LC11', 'registered': true},
-        {'id': 10, 'name': '김학산', 'phoneNumber': '5555', 'LC': 'LC11', 'registered': false}
-    ]
-
-    const checkboxKey = 'registered'
-    const [tableData, updateTableData] = React.useState(userInfo)
-    const [searchData, setSearchData] = React.useState(userInfo)
+    const checkboxKey = 'register'
+    const [tableData, updateTableData] = React.useState([])
+    const [searchData, setSearchData] = React.useState([])
     const [searchQuery, setSearchQuery] = React.useState('')
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                setLoading(true)
+                const res = await api.getFreshmanList()
+                updateTableData(res.data.data)
+                setSearchData(res.data.data)
+            } catch (err) {
+            }
+            setLoading(false)
+        };
+        fetchUsers();
+    }, []);
+
+    const tableHeader = {'#': 'index', 
+                        '이름': 'name', 
+                        '전화번호': 'phone_number', 
+                        'LC': 'lc', 
+                        '접수': 'register'}
 
     const onChangeSearch = (query) => {
         setSearchQuery(query)
@@ -94,6 +99,7 @@ export default function RegisterScreen() {
                         data={tableData}
                         checkboxKey={checkboxKey}
                         toggleFunc={Register}
+                        loading={loading}
                     />
                 </View>   
             </View>
