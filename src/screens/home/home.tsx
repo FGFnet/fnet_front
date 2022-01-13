6faada616aba90a0f3a947c7fc65444045d52c79
goo314 <rn0731@naver.com>
2022-01-14 01:39:06 +0900

    fix: Add API

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,17 +1,72 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Image, StyleSheet, ScrollView} from 'react-native';
 import {Text, Button, Divider} from 'react-native-paper';
 import {Colors} from '../../constants';
 import { basicStyles, GreenButton, Header } from '../../components';
 import {useNavigation} from '../../providers';
+import api from '../../utils/api'
 
 export default function HomeScreen() {
     const navigation = useNavigation();
 
+    const [fgData, setFGData] = useState([]) // FG info
+    const [lcData, setLCData] = useState([]) // LC list of FG
+    const [firstLCData, setFirstLCData] = useState({})
+
+    const [totalRegister, setTotalRegister] = useState(0)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchUsers = async() => {
+            try {
+                setLoading(true)
+                const res_fg = await api.getFG()
+                setFGData(res_fg.data.data)
+                const res_lc = await api.getLCList()
+                setLCData(res_lc.data.data)
+                if (res_lc.data.length == 0){ // empty LC list
+                    setFirstLCData([{'name': 'None', 'schedule': '/'}])
+                    setTotalRegister(0)
+                } else{
+                    const firstDate = lcData[0]['schedule'].split('-')
+                    setFirstLCData({"name": lcData[0]['name'], "schedule": +firstDate[1] + '/' + +firstDate[2]})
+                    const res_register = await api.getLCMemberList(firstLCData['id'], true)
+                    setTotalRegister(res_register.data.length)
+                }
+            } catch(err) {
+
+            }
+            setLoading(false)
+        };
+        fetchUsers();
+    })
+
+    
+    const is_admin = () => {
+        if (fgData['is_admin']){
+            return 1
+        } else {
+            return 0
+        }
+    }
+
+    var otSchedule = lcData.map((lc) =>
+        <View key = {lc['id']} style={styles.schedule}>
+            <Text style={{paddingRight: 20}}>
+                {lc['schedule']}
+            </Text>
+            <GreenButton
+                text={lc['name']}
+                press={()=>alert('LC09')}
+            />
+        </View>
+    )
+
+    if (loading) return (<Text>Loading...</Text>)
     return(
         <ScrollView>
             <Button
-                style={{position: 'absolute', bottom:0, right:0, zIndex: 1}}
+                style={{position: 'absolute', bottom:0, right:0, zIndex: is_admin()}}
                 icon="settings-outline"
                 onPress={() => {
                     navigation.navigate('Setting', {})
@@ -25,9 +80,9 @@ export default function HomeScreen() {
                     resizeMode='contain'
                 />
                 <View style={styles.headerFgInfo}>
-                    <Text style={styles.headerFgName}>김하늘 FG</Text>
-                    <Text style={basicStyles.contentText}>2/14 진행 LC  LC09</Text>
-                    <Text>LC09 접수 인원  15</Text>
+                    <Text style={styles.headerFgName}>{fgData['name']} FG</Text>
+                    <Text style={basicStyles.contentText}> {firstLCData['schedule']} 진행 LC {firstLCData['name']}</Text>
+                    <Text>{firstLCData['name']} 접수 인원  {totalRegister}</Text>
                 </View>
             </View>
             
@@ -40,7 +95,7 @@ export default function HomeScreen() {
                         navigation.navigate('RegisterList', {});
                     }}
                 >
-                    <Text style={{fontSize: 14, color: Colors.primary}}>LC 09</Text>
+                    <Text style={{fontSize: 14, color: Colors.primary}}>{firstLCData['name']}</Text>
                 </Button>
                 <Button
                     icon="chatbubbles-outline"
@@ -87,32 +142,8 @@ export default function HomeScreen() {
                         }}
                     > </Button>
                 </View>
-                <View style={basicStyles.insideContainer}>
-                    <View style={styles.schedule}>
-                        <Text style={{paddingRight: 20}}>
-                            2021.02.14
-                        </Text>
-                        <GreenButton
-                            text='LC09'
-                            press={()=>alert('LC09')}
-                        />
-                    </View>
-                    <View style={styles.schedule}>
-                        <Text style={{paddingRight: 20}}>
-                            2021.02.15
-                        </Text>
-                        <GreenButton
-                            text='LC57'
-                        />
-                    </View>
-                    <View style={styles.schedule}>
-                        <Text style={{paddingRight: 20}}>
-                            2021.02.16
-                        </Text>
-                        <GreenButton
-                            text='LC88'
-                        />
-                    </View>
+                <View style={basicStyles.insideContainer}> 
+                   {otSchedule}
                 </View>
             </View>
         </ScrollView>
--- src/screens/lcSetting/lcSetting.tsx
+++ src/screens/lcSetting/lcSetting.tsx
@@ -1,19 +1,87 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, ScrollView, Pressable, TouchableOpacity} from 'react-native';
 import {Text} from 'react-native-paper';
 import { basicStyles, GreenButton, Header, InputForm } from '../../components';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import api from '../../utils/api';
 
 export default function LCSettingScreen() {
     const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
     const [selectDay, setSelectDay] = useState("")
 
-    const [firstDay, onChangeFirstDay] = useState("");
-    const [secondDay, onChangeSecondDay] = useState("");
-    const [thirdDay, onChangeThirdDay] = useState("");
-    const [firstLC, setFirstLC] = useState("");
-    const [secondLC, setSecondLC] = useState("");
-    const [thirdLC, setThirdLC] = useState("");
+    // Original LC information
+    const [firstLC, setFirstLC] = useState({"name":"", "schedule":""});
+    const [secondLC, setSecondLC] = useState({"name":"", "schedule":""});
+    const [thirdLC, setThirdLC] = useState({"name":"", "schedule":""});
+
+    // Change LC information
+    const [newFirstLC, onChangeFirstLC] = useState({"name":"", "schedule":""})
+    const [newSecondLC, onChangeSecondLC] = useState({"name":"", "schedule":""})
+    const [newThirdLC, onChangeThirdLC] = useState({"name":"", "schedule":""})
+
+    const [lcList, setLCList] = useState([])
+    const [loading, setLoading] = useState(false);
+
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                setLoading(true)
+                const res = await api.getLCList()
+                setLCList(res.data.data)
+                for (let i=0; i<lcList.length; i++){
+                    if (i == 0) {
+                        setFirstLC({"name":lcList[0]['name'], "schedule": lcList[0]['schedule']})
+                        onChangeFirstLC({"name":lcList[0]['name'], "schedule": lcList[0]['schedule']})
+                    }
+                    else if (i == 1) {
+                        setSecondLC({"name":lcList[0]['name'], "schedule": lcList[0]['schedule']})
+                        onChangeSecondLC({"name":lcList[0]['name'], "schedule": lcList[0]['schedule']})
+                    }
+                    else {
+                        setThirdLC({"name":lcList[0]['name'], "schedule": lcList[0]['schedule']})
+                        onChangeThirdLC({"name":lcList[0]['name'], "schedule": lcList[0]['schedule']})
+                    }
+                }
+            } catch (err) {
+            }
+            setLoading(false)
+        };
+        fetchUsers();
+    })
+
+    async function updateLCSetting() {
+        try{
+            if(firstLC['name']!=="" && firstLC['name']!==newFirstLC['name']){
+                alert('You can not change LC')
+                return;
+            }
+            if(secondLC['name']!=="" && secondLC['name']!==newSecondLC['name']){
+                alert('You can not change LC')
+                return;
+            }
+            if(thirdLC['name']!=="" && thirdLC['name']!==newThirdLC['name']){
+                alert('You can not change LC')
+                return;
+            }
+
+            const res_first = await api.getLC({"name":newFirstLC['name']})
+            if(res_first.data.data.length > 1) await api.createLC(newFirstLC)
+            else await api.editLC(newFirstLC)
+            
+            const res_second = await api.getLC({"name":newSecondLC['name']})
+            if(res_second.data.data.length > 1) await api.createLC(newSecondLC)
+            else await api.editLC(newSecondLC)
+
+            const res_third = await api.getLC({"name":newThirdLC['name']})
+            if(res_third.data.data.length > 1) await api.createLC(newThirdLC)
+            else await api.editLC(newThirdLC)
+
+        } catch (err) {
+
+        }
+        location.reload()
+    }
 
     const showDatePicker = (selectDay) => {
         setDatePickerVisibility(true);
@@ -26,11 +94,12 @@ export default function LCSettingScreen() {
 
     const handleConfirm = (date) => {
         hideDatePicker()
-        if (selectDay === '1') onChangeFirstDay(date.format("yyyy/MM/dd"))
-        else if (selectDay === '2') onChangeSecondDay(date.format("yyyy/MM/dd"))
-        else onChangeThirdDay(date.format("yyyy/MM/dd"))
+        if (selectDay === '1') onChangeFirstLC({"name":newFirstLC['name'], "schedule":date.format("yyyy/MM/dd")})
+        else if (selectDay === '2') onChangeSecondLC({"name":newSecondLC['name'], "schedule":date.format("yyyy/MM/dd")})
+        else onChangeThirdLC({"name":newThirdLC['name'], "schedule":date.format("yyyy/MM/dd")})
     }
 
+    if (loading) return (<Text>Loading...</Text>)
     return(
         <ScrollView>
             <View style={basicStyles.container}>
@@ -50,7 +119,7 @@ export default function LCSettingScreen() {
                         <InputForm
                             placeholder='날짜'
                             editable={false}
-                            value={firstDay}
+                            value={newFirstLC['schedule']}
                             height={40}
                             pointerEvents="none"
                             style={{width: 120}}
@@ -72,7 +141,7 @@ export default function LCSettingScreen() {
                         <InputForm
                             placeholder='날짜'
                             editable={false}
-                            value={secondDay}
+                            value={newSecondLC['schedule']}
                             height={40}
                             pointerEvents="none"
                             style={{width: 120}}
@@ -94,7 +163,7 @@ export default function LCSettingScreen() {
                         <InputForm
                             placeholder='날짜'
                             editable={false}
-                            value={thirdDay}
+                            value={newThirdLC['schedule']}
                             height={40}
                             pointerEvents="none"
                             style={{width: 120}}
@@ -114,6 +183,7 @@ export default function LCSettingScreen() {
                         flex: 1, 
                         alignItems: 'flex-end'
                     }}
+                    onClick={updateLCSetting}
                 />
             </View>
         </ScrollView>
